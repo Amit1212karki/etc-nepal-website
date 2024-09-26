0097c6b863cf47f7f0d0053f2282bbfddb514d67
sonu dhakal <sonudhakal010@gmail.com>
2024-09-26 15:24:42 +0545

    changes

--- certificate/urls.py
+++ certificate/urls.py
@@ -4,4 +4,16 @@ from .views import *
 urlpatterns = [
      path("", login, name="certificate-login"),
      path("dashboard/", home, name="certificate-home"),
+     path("contract/", contractIndex, name="contract-index"),
+     path("contract/add/", contractCreate, name="contract-create"),
+     path("contract/edit/", contractEdit, name="contract-edit"),
+     path("batch/", batchIndex, name="batch-index"),
+     path("batch/add/", batchCreate, name="batch-create"),
+     path("batch/edit/", batchEdit, name="batch-edit"),
+     path("trainer/", trainerIndex, name="trainer-index"),
+     path("trainer/add/", trainerCreate, name="trainer-create"),
+     path("trainer/edit/", trainerEdit, name="trainer-edit"),
+     path("trainee/", traineeIndex, name="trainee-index"),
+     path("trainee/add/", traineeCreate, name="trainee-create"),
+     path("trainee/edit/", traineeEdit, name="trainee-edit"),
 ]
--- certificate/views.py
+++ certificate/views.py
@@ -6,3 +6,44 @@ def login(request):
 
 def home(request):
     return render(request, "back/home.html")
+
+def contractIndex(request):
+    return render(request, "back/contract/index.html")
+
+def contractEdit(request):
+    return render(request, "back/contract/edit.html")
+
+def contractCreate(request):
+    return render(request, "back/contract/add.html")
+
+
+def batchIndex(request):
+    return render(request, "back/batch/index.html")
+
+def batchEdit(request):
+    return render(request, "back/batch/edit.html")
+
+def batchCreate(request):
+    return render(request, "back/batch/add.html")
+
+
+
+def trainerIndex(request):
+    return render(request, "back/trainer/index.html")
+
+def trainerEdit(request):
+    return render(request, "back/trainer/edit.html")
+
+def trainerCreate(request):
+    return render(request, "back/trainer/add.html")
+
+
+
+def traineeIndex(request):
+    return render(request, "back/trainee/index.html")
+
+def traineeEdit(request):
+    return render(request, "back/trainee/edit.html")
+
+def traineeCreate(request):
+    return render(request, "back/trainee/add.html")
