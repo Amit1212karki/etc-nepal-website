17eae2dacdecd6c876f28e0c32a1febd8bb4ad10
amit1212karki <amitkarki89@gmail.com>
2024-10-04 13:41:53 +0545

    char

--- certificate/urls.py
+++ certificate/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
      path('logout/<int:id>/', logoutUser, name='certificate-logout'),
      path('dashboard/', home, name='certificate-home'),
      
+     # certificate generation urls
+     path('certificate-index/', certificatIndex, name='certificate-index'),
      path("certificate-form/", certificateForm ),
      path("certificate-form2/", certificateForm2 ),
+     path("html-to-pdf/", pdf_view),
+     path("html-to-pdf2/", pdf_view2),
+
     
 ]
--- etcportfolio/urls.py
+++ etcportfolio/urls.py
@@ -43,11 +43,9 @@ urlpatterns = [
     path('contract/', include('contract.urls')),
     path('trainer/', include('trainer.urls')),
     path('batch/', include('batch.urls')),
-    path("signatory/", include("signatory.urls")),
-    path("trainee/", include("trainee.urls")),
-    path("html-to-pdf/", pdf_view),
-    path("html-to-pdf2/", pdf_view2),
-
+    path('signatory/', include('signatory.urls')),
+    path('trainee/', include('trainee.urls')),
+    
 ]
 
 if settings.DEBUG:
